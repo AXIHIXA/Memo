72fcc209f287699e2f3ff881aa609138e1d54230
Xi Han <ahanxi@126.com>
2019-11-10 22:45:12 -0500

    Update

---
+++ code/pipe_demo.c
@@ -0,0 +1,54 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <wait.h>
+
+#define READ_END     0
+#define WRITE_END    1
+
+const char firstcmd[] = "ls";
+const char frsarg[] = "-la";
+const char scmd[] = "wc";
+const char secarg[] = "-l";
+
+int main()
+{
+    pid_t pid;
+    int fd[2];
+
+    pipe(fd);
+    pid = fork();
+
+    if (pid == 0)
+    {
+        dup2(fd[WRITE_END], STDOUT_FILENO);
+        close(fd[READ_END]);
+        close(fd[WRITE_END]);
+        execlp(firstcmd, firstcmd, frsarg, (char *) NULL);
+        fprintf(stderr, "Failed to execute '%s'\n", firstcmd);
+        exit(1);
+    }
+    else
+    {
+        pid = fork();
+
+        if (pid == 0)
+        {
+            dup2(fd[READ_END], STDIN_FILENO);
+            close(fd[WRITE_END]);
+            close(fd[READ_END]);
+            execlp(scmd, scmd, secarg, (char *) NULL);
+            fprintf(stderr, "Failed to execute '%s'\n", scmd);
+            exit(1);
+        }
+        else
+        {
+            int status;
+            close(fd[READ_END]);
+            close(fd[WRITE_END]);
+            waitpid(pid, &status, 0);
+        }
+    }
+}
