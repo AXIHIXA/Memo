c06154f7fc40aeca1ebbb908ad157b72990aba8e
Xi Han <ahanxi@126.com>
2020-04-02 00:47:40 -0400

    Update

--- code/debug.h
+++ code/debug.h
@@ -1,9 +1,9 @@
-#ifndef DEBUG_H
-#define DEBUG_H
+#ifndef ST_DEBUG_H
+#define ST_DEBUG_H
 
-#include <stdio.h>
 
-#define ENDL "\n"
+#include <cstdio>
+
 
 #ifdef ST_COLOR
 #define KNRM "\033[0m"
@@ -27,6 +27,14 @@
 #define KBWN ""
 #endif
 
+
+#ifdef ST_NEWLINE
+#define ENDL "\n"
+#else
+#define ENDL ""
+#endif
+
+
 #ifdef ST_VERBOSE
 #define ST_DEBUG
 #define ST_INFO
@@ -35,64 +43,70 @@
 #define ST_SUCCESS
 #endif
 
+
 #ifdef ST_DEBUG
 #define st_debug(S, ...)                                                           \
     do                                                                             \
     {                                                                              \
-        fprintf(stderr, KMAG "DEBUG: %s %s:%d " KNRM S ENDL,                       \
-                __extension__ __FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__);    \
+        fprintf(stdout, KMAG "[DEBUG] %s:%d " KNRM S ENDL,                         \
+                __FILE__, __LINE__, ##__VA_ARGS__);                                \
     }                                                                              \
     while (0)
 #else
 #define st_debug(S, ...)
 #endif
 
+
 #ifdef ST_INFO
 #define st_info(S, ...)                                                            \
     do                                                                             \
     {                                                                              \
-        fprintf(stderr, KBLU "INFO: %s %s:%d " KNRM S ENDL,                        \
-                __extension__ __FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__);    \
+        fprintf(stdout, KBLU "[INFO] %s:%d " KNRM S ENDL,                          \
+                __FILE__, __LINE__, ##__VA_ARGS__);                                \
     }                                                                              \
     while (0)
 #else
 #define st_info(S, ...)
 #endif
 
+
 #ifdef ST_WARN
 #define st_warn(S, ...)                                                            \
     do                                                                             \
     {                                                                              \
-        fprintf(stderr, KYEL "WARN: %s %s:%d " KNRM S ENDL,                        \
-                __extension__ __FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__);    \
+        fprintf(stdout, KYEL "[WARN] %s:%d " KNRM S ENDL,                          \
+                __FILE__, __LINE__, ##__VA_ARGS__);                                \
     }                                                                              \
     while (0)
 #else
 #define st_warn(S, ...)
 #endif
 
+
 #ifdef ST_SUCCESS
 #define st_success(S, ...)                                                         \
     do                                                                             \
     {                                                                              \
-        fprintf(stderr, KGRN "SUCCESS: %s %s:%d " KNRM S ENDL,                     \
-                __extension__ __FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__);    \
+        fprintf(stdout, KGRN "[SUCCESS] %s:%d " KNRM S ENDL,                       \
+                __FILE__, __LINE__, ##__VA_ARGS__);                                \
     }                                                                              \
     while (0)
 #else
 #define st_success(S, ...)
 #endif
 
+
 #ifdef ST_ERROR
 #define st_error(S, ...)                                                           \
     do                                                                             \
     {                                                                              \
-        fprintf(stderr, KRED "ERROR: %s %s:%d " KNRM S ENDL,                       \
-                __extension__ __FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__);    \
+        fprintf(stdout, KRED "[ERROR] %s:%d " KNRM S ENDL,                         \
+                __FILE__, __LINE__, ##__VA_ARGS__);                                \
     }                                                                              \
     while (0)
 #else
 #define st_error(S, ...)
 #endif
 
-#endif /* DEBUG_H */
+
+#endif  // ST_DEBUG_H
