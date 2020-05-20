e3c8e29800b07336338656bb82b4a74a9d935850
Xi Han <ahanxi@126.com>
2020-05-20 17:00:58 -0400

    "{}"

--- update.sh
+++ update.sh
@@ -1,3 +1,13 @@
-git add -A &&
-git commit -m "\"{$1}\"" &&
-git push
+if $1 
+then
+    git add -A &&
+    git commit -m "\"{$1}\"" &&
+    git push
+else
+    git add -A &&
+    git commit -m "Update" &&
+    git push
+fi
+
+
+
