bf90154992bdb80d0774a9942d856058b24993cc
Xi Han <ahanxi@126.com>
2020-06-16 01:22:19 -0400

    Update

--- code/variadic.cpp
+++ code/variadic.cpp
@@ -2,6 +2,8 @@
 #include <bits/stdc++.h>
 
 
+// variadic template class expansion
+// via partial specification
 template <typename T>
 struct Sum<T>
 {
@@ -10,7 +12,6 @@ struct Sum<T>
         value = sizeof(T)
     };
 };
-
 
 template <typename T, typename ... Args>
 struct Sum
@@ -22,18 +23,18 @@ struct Sum
 };
 
 
+// variadic template class expansion
+// via inheritance
 template <int ...>
 struct IndexSeq
 {
 
 };
 
-
 template <int N, int ... Indexes>
 struct MakeIndexes : MakeIndexes<N - 1, N - 1, Indexes ...>
 {
 };
-
 
 template <int ... Indexes>
 struct MakeIndexes<0, Indexes ...>
@@ -42,12 +43,13 @@ struct MakeIndexes<0, Indexes ...>
 };
 
 
+// variadic template function expansion
+// via recursion
 template<typename T>
 void pf1(T && t)
 {
     std::cout << t << std::endl;
 }
-
 
 template<typename T, typename ... Args>
 void pf1(T && t, Args && ... args)
@@ -57,6 +59,8 @@ void pf1(T && t, Args && ... args)
 }
 
 
+// variadic template function expansion
+// via initializer list of comma expressions
 template <typename ... Args>
 void pf2(Args && ... args)
 {
@@ -65,6 +69,8 @@ void pf2(Args && ... args)
 }
 
 
+// a fake "variadic variable" function
+// can be done by passing in an initializer list
 template <typename T>
 void pf3(std::initializer_list<T> il)
 {
@@ -74,6 +80,43 @@ void pf3(std::initializer_list<T> il)
     }
 
     std::cout << std::endl;
+}
+
+
+// a test on perfect forwarding of variadic template functions
+
+// variadic template function expansion
+// via recursion
+template <typename T>
+void fun4(T && t)
+{
+    std::cout << "void fun3(T && t) " << t << std::endl;
+}
+
+void fun4(std::string && s)
+{
+    std::cout << "void fun3(std::string && s) " << s << std::endl;
+}
+
+template <typename T, typename ... Args>
+void fun4(T && t, Args && ... args)
+{
+    std::cout << "void fun4(T && t, Args && ... args) " << t << std::endl;
+    fun4(std::forward<Args>(args) ...);
+}
+
+// this one does perfect forwarding, successfully calling the std::string && specification 
+template <typename ... Args>
+void fun3_1(Args && ... args)
+{
+    fun4(std::forward<Args>(args) ...);
+}
+
+// this one doesn't do perfect forwarding, only calling the template
+template <typename ... Args>
+void fun3_2(Args && ... args)
+{
+    fun4(args ...);
 }
 
 
@@ -88,6 +131,12 @@ int main(int argc, char * argv[])
     pf2(0, 1, 2, 3, 4);
 
     pf3({0, 1, 2, 3, 4});
+    
+    fun3_1(1, std::string {"rval"});  // void fun4(T && t, Args && ... args) 1
+                                      // void fun3(std::string && s) rval
+                              
+    fun3_2(1, std::string {"rval"});  // void fun4(T && t, Args && ... args) 1
+                                      // void fun3(T && t) rval
 
     return EXIT_SUCCESS;
 }
