e1f600c456ee2de43be699ad889313e11c96f0bd
Xi Han <ahanxi@126.com>
2023-11-17 18:44:43 -0500

    Update

--- code/CMakeLists/PTE/src/main.cu
+++ code/CMakeLists/PTE/src/main.cu
@@ -7,7 +7,34 @@ torch::Tensor add_one(const torch::Tensor & t)
 }
 
 
+/// @brief Batch-wise 2D cross product.
+/// @param a Tensor of shape (batch, 2).
+/// @param b Tensor of shape (batch, 2).
+/// @return  Tensor of shape (batch, 1).
+torch::Tensor cross2d(const torch::Tensor & a, const torch::Tensor & b)
+{
+    try
+    {
+        torch::Tensor c = torch::ones({1, 2}).to(a.device());
+        c.index_put_({0, 1}, -1);
+        return torch::sum(a * b.roll(1, 1) * c, -1);
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << e.what() << '\n';
+        throw e;
+    }
+    catch (...)
+    {
+        std::cerr << "Caught whatever ..." << '\n';
+        throw;
+    }
+}
+
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-    m.def("add_one", &add_one, py::arg("t"));
+    using py::literals::operator""_a;
+    m.def("add_one", add_one, "t"_a);
+    m.def("cross2d", cross2d, "a"_a, "b"_a);
 }
--- code/CMakeLists/PTE/test.py
+++ code/CMakeLists/PTE/test.py
@@ -4,11 +4,15 @@ import build.pte as pte
 
 
 def main() -> None:
-    device: torch.device = torch.device('cpu')
-    g: torch.Generator = torch.Generator(device=device)
-    a: torch.Tensor = torch.rand((11,), generator=g, dtype=torch.float32, device=device)
+    device: torch.device = torch.device('cuda')
+    # g: torch.Generator = torch.Generator(device=device)
+    # a: torch.Tensor = torch.rand((3, 2), generator=g, dtype=torch.float32, device=device)
+    a: torch.Tensor = torch.tensor([[1, 2], [3, 4], [5, 6]], dtype=torch.float32, device=device)
     print(a)
-    print(pte.add_one(a))
+    b = pte.add_one(a)
+    print(b)
+    c = pte.cross2d(a, b)
+    print(c)
 
 
 if __name__ == '__main__':
