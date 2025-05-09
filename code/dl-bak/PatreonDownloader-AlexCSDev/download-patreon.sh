7a9208d129fbe58fded51695fd03b5227b19c873
Xi Han <xihan1997@gmail.com>
2025-05-08 23:28:19 -0400

    Update

--- code/dl-bak/PatreonDownloader-AlexCSDev/download-patreon.sh
+++ code/dl-bak/PatreonDownloader-AlexCSDev/download-patreon.sh
@@ -1,4 +1,10 @@
 /mnt/e/opt/PatreonDownloader-AlexCSDev/PatreonDownloader.App.exe \
 --url "https://www.patreon.com/c/KK_pixiv/posts" \
 --download-directory "/AX/ETC/MARCH OF THE PENGUINS/CG/KK_pixiv" \
---file-exists-action "ReplaceIfDifferent" 
+--file-exists-action "ReplaceIfDifferent" \
+--use-sub-directories
+
+/mnt/e/opt/PatreonDownloader-AlexCSDev/PatreonDownloader.App.exe \
+--url "https://www.patreon.com/c/KK_pixiv/posts" \
+--download-directory "/AX/ETC/MARCH OF THE PENGUINS/CG/KK_pixiv_exp" \
+--file-exists-action "ReplaceIfDifferent"
