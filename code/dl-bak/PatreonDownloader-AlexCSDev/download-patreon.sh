1f50d727dac321007dc0b1cda84db36a5fe21b78
Xi Han <xihan1997@gmail.com>
2025-07-17 23:36:38 -0400

    Update

--- code/dl-bak/PatreonDownloader-AlexCSDev/download-patreon.sh
+++ code/dl-bak/PatreonDownloader-AlexCSDev/download-patreon.sh
@@ -1,13 +1,14 @@
-# --download-directory is RELATIVE path to current working directory, i.e., that of the Windows Terminal WSL!
+# --download-directory is not really working for ubuntu release.
+# --download-directory "/Hfhf/CG-AI/KK_pixiv" invoked at /mnt/f/ results in /mnt/f/Hfhf/CG-AI/KK_pixiv.
 # Invoke at root of F:/!
 
 /mnt/e/opt/PatreonDownloader-AlexCSDev/PatreonDownloader.App.exe \
 --url "https://www.patreon.com/c/KK_pixiv/posts" \
---download-directory "Hfhf/CG-AI/KK_pixiv" \
+--download-directory "/Hfhf/CG-AI/KK_pixiv" \
 --file-exists-action "ReplaceIfDifferent" \
 --use-sub-directories
 
 /mnt/e/opt/PatreonDownloader-AlexCSDev/PatreonDownloader.App.exe \
 --url "https://www.patreon.com/c/KK_pixiv/posts" \
---download-directory "Hfhf/CG-AI/KK_pixiv_exp" \
+--download-directory "/Hfhf/CG-AI/KK_pixiv_exp" \
 --file-exists-action "ReplaceIfDifferent"
